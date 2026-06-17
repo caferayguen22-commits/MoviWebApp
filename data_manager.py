b8cf8cd3e87e78f688997bc67343f48ce9480fb5
caferayguen22-commits <cafer.ayguen22@gmail.com>
2026-06-17 19:22:00 +0200

    Refactor: Add try-except error handling and update queries to session style in DataManager

--- data_manager.py
+++ data_manager.py
@@ -4,33 +4,55 @@ class DataManager:
 
     def create_user(self, name):
         """Fügt einen neuen Nutzer zur Datenbank hinzu."""
-        new_user = User(name=name)
-        db.session.add(new_user)
-        db.session.commit()
+        try:
+            new_user = User(name=name)
+            db.session.add(new_user)
+            db.session.commit()
+            return True
+        except Exception:
+            db.session.rollback()
+            return False
 
     def get_user(self):
         """Gibt eine Liste aller registrierten Nutzer zurück"""
-        return User.query.all()
+        return db.session.query(User).all()
 
     def get_movies(self, user_id):
         """Gibt alle Lieblingsfilme eines bestimmten Nutzers zurück."""
-        return Movie.query.filter_by(user_id=user_id).all()
+        return db.session.query(Movie).filter_by(user_id=user_id).all()
 
     def add_movie(self, movie):
         """Fügt ein bereits erstelltes Movie_Objekt zur Datenbank hinzu."""
-        db.session.add(movie)
-        db.session.commit()
+        try:
+            db.session.add(movie)
+            db.session.commit()
+            return True
+        except Exception:
+            db.session.rollback()
+            return False
 
     def update_movie(self, movie_id, new_title):
         """Aktualisiert ein Titel eines bestimmten Films anhand seiner ID."""
-        movie = Movie.query.get(movie_id)
-        if movie:
-            movie.title = new_title
-            db.session.commit()
+        try:
+            movie = db.session.get(Movie, movie_id)
+            if movie:
+                movie.title = new_title
+                db.session.commit()
+                return True
+            return False
+        except Exception:
+            db.session.rollback()
+            return False
 
     def delete_movie(self, movie_id):
         """Löscht einen Film komplett aus der Datenbank."""
-        movie = Movie.query.get(movie_id)
-        if movie:
-            db.session.delete(movie)
-            db.session.commit()
+        try:
+            movie = db.session.get(Movie, movie_id)
+            if movie:
+                db.session.delete(movie)
+                db.session.commit()
+                return True
+            return False
+        except Exception:
+            db.session.rollback()
+            return False
